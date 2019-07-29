64d4c2d6f664ef14903b2a47854baf262ce81b56
alexandr <alexandrov6927@gmail.com>
2019-07-29 15:41:00 +0300

    Updates

---
+++ precache-manifest.81f809c395896bf25c7c79f4d20370cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6498ecec890a34ddf6511c436d3a886",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "92a8297d815ace0fb130",
+    "url": "/react_tabs/static/css/main.70c3822e.chunk.css"
+  },
+  {
+    "revision": "522c2e1b3f687a3c6fcb",
+    "url": "/react_tabs/static/js/2.3700d4c4.chunk.js"
+  },
+  {
+    "revision": "92a8297d815ace0fb130",
+    "url": "/react_tabs/static/js/main.4854f90c.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
